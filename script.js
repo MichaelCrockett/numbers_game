ba48c86e560353af6e438ed1c7f0a9c55033d6d7
Michael Crockett <mike.crockett76@gmail.com>
2019-12-09 15:02:24 +0000

    Game complete

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ var userGuessLog =[];
 var attempts = 0;
 var maxGuesses = 10;
 
+function gameEnded() {
+  document.getElementById('newGameButton').style.display = 'inline';
+  document.getElementById('easyBtn').style.display = 'none';
+  document.getElementById('hardBtn').style.display = 'none';
+  document.getElementById('inputBox').setAttribute('readonly', 'readonly')
+
+}
+
 function easyMode() {
   maxGuesses = 10;
+  document.getElementById('easyBtn').className = 'activeButton';
+  document.getElementById('hardBtn').className = '';
 }
 
 function hardMode() {
   maxGuesses = 5;
+  document.getElementById('easyBtn').className = '';
+  document.getElementById('hardBtn').className = 'activeButton';
 }
 
 function newGame() {
@@ -18,16 +30,16 @@ function newGame() {
 
 function init() {
   initialNumber = Math.floor(Math.random() *100 + 1);
-  // console.log(initialNumber);
+  console.log(initialNumber);
   document.getElementById('newGameButton').style.display = 'none';
 }
 
 function compareGuess() {
   var userGuess =" " + document.getElementById('inputBox').value;
-  // console.log(userGuess)
+  console.log(userGuess)
 
  userGuessLog.push(userGuess);
- // console.log(userGuessLog);
+ console.log(userGuessLog);
  document.getElementById("guessLog").innerHTML = userGuessLog;
 
  attempts++;
@@ -42,15 +54,23 @@ function compareGuess() {
       document.getElementById("textOutput").innerHTML = "Guess is too low";
       document.getElementById("inputBox").value ="";
     } else {
-      document.getElementById("textOutput").innerHTML = "Correct";
+      document.getElementById("textOutput").innerHTML = "Correct in " +attempts+ " attempts";
+      document.getElementById('container').style.backgroundColor = 'DarkGreen'
+      gameEnded();
     }
   } else {
     if(userGuess > initialNumber) {
-      document.getElementById('textOutput').innerHTML = "You have no more guesses";
+      document.getElementById('textOutput').innerHTML = "You have no more guesses  <br> The correct answer was " + initialNumber
+      document.getElementById('container').style.backgroundColor = 'DarkGreen'
+      gameEnded();
     } else if (userGuess < initialNumber) {
-      document.getElementById('textOutput').innerHTML = "ou have no more guesses";
+      document.getElementById('textOutput').innerHTML = "You have no more guesses <br> The correct answer was " + initialNumber ;
+      document.getElementById('container').style.backgroundColor = 'DarkGreen'
+      gameEnded();
     } else {
-      document.getElementById('textOutput').innerHTML = "Correct";
+      document.getElementById('textOutput').innerHTML = "Correct in " +attempts+ " attempts";
+      document.getElementById('container').style.backgroundColor = 'DarkGreen'
+      gameEnded();
     }
   }
 
